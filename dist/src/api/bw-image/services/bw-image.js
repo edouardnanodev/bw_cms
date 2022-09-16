fc95efa8987a434451d7742a5c30d1db63b49ba8
billsomen <eduard.yonga@nanodev.ca>
2022-09-16 10:55:16 +0100

    added bw_images

---
+++ dist/src/api/bw-image/services/bw-image.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * bw-image service.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::bw-image.bw-image');
---
+++ src/api/bw-image/services/bw-image.ts
@@ -0,0 +1,7 @@
+/**
+ * bw-image service.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::bw-image.bw-image');
