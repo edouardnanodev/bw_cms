1f1f610911eb373011a8fb2cca381fb1295b8629
miloxnanodev <thierry.lontsi@nanodev.ca>
2023-01-25 11:09:28 +0100

    Updated to Strapi 4.5.6 and Added Carelic collection type

---
+++ dist/src/api/carelic-string/routes/carelic-string.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * carelic-string router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::carelic-string.carelic-string');
---
+++ src/api/carelic-string/controllers/carelic-string.ts
@@ -0,0 +1,7 @@
+/**
+ * carelic-string controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::carelic-string.carelic-string');
